a168767d6ae4d6fcfa2b78ebff8b7fee27c47998
Annise Arif <83833939+annise-arif@users.noreply.github.com>
2022-04-18 21:44:38 +0600

    implemented about page and updated services section

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -14,7 +14,11 @@ const About = () => {
           src={me}
         />
         <Figure.Caption>
-          Nulla vitae elit libero, a pharetra augue mollis interdum.
+        . my dream is will be a good web developer as a full stack developer 
+         because this in my one of my carrier goals. <br />
+          and I hope it will be my future profession  and I know that I have to be very hard work for my dream next 3month, <br />
+         . and my target is within 4monh I want to get a good job or intern at any software company. <br />
+         I want to do very hard work for my dream. and want to go to soo far to web development carrier,
         </Figure.Caption>
       </Figure>
     </div>
